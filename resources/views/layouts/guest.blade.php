88e755a42eeccd255052484489f833c971830ac8
KitMarjohn <mjbolwar@gmail.com>
2026-01-08 02:28:07 +0800

    fixed auth pages mobile view

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,12 +16,13 @@
     </head>
     <body class="font-sans text-gray-900 antialiased bg-white">
         
-        <div class="min-h-screen flex">
+        {{-- WRAPPER: Flex Column on Mobile, Row on Desktop --}}
+        <div class="min-h-screen flex flex-col lg:flex-row">
             
-            {{-- LEFT SIDE: BRANDING PANEL (Hidden on Mobile, Visible on Desktop) --}}
+            {{-- DESKTOP SIDEBAR (Left - Hidden on Mobile) --}}
             <div class="hidden lg:flex lg:w-1/2 bg-gradient-to-br from-maroon-700 to-maroon-900 text-white flex-col justify-between p-12 relative overflow-hidden">
                 
-                {{-- Background Pattern (Subtle) --}}
+                {{-- Background Pattern (Subtle SVG) --}}
                 <div class="absolute inset-0 opacity-10">
                     <svg class="h-full w-full" width="100%" height="100%" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
                         <defs><pattern id="grid" width="40" height="40" patternUnits="userSpaceOnUse"><path d="M0 40L40 0H20L0 20M40 40V20L20 40" stroke="currentColor" stroke-width="2" fill="none"/></pattern></defs>
@@ -31,8 +32,8 @@
 
                 {{-- Content --}}
                 <div class="relative z-10">
-                    <a href="/" class="block">
-                         {{-- Logo: We force white fill via text-white class on the component --}}
+                    {{-- FIXED: Use route('home') instead of "/" --}}
+                    <a href="{{ route('home') }}" class="block">
                         <x-application-logo class="w-20 h-20 fill-current text-white mb-6" />
                     </a>
                     <h1 class="text-5xl font-bold tracking-tight mb-4">Think. Ask. Learn.</h1>
@@ -46,21 +47,28 @@
                 </div>
             </div>
 
-            {{-- RIGHT SIDE: FORM PANEL --}}
-            <div class="w-full lg:w-1/2 flex flex-col justify-center items-center p-8 bg-white overflow-y-auto relative">
-                
-                {{-- Mobile Logo (Visible only on small screens) --}}
-                <div class="lg:hidden mb-8">
-                    <a href="/">
-                        <x-application-logo class="w-16 h-16 fill-current text-maroon-700" />
-                    </a>
+            {{-- MOBILE HEADER (Top - Visible only on Mobile) --}}
+            <div class="lg:hidden w-full bg-gradient-to-br from-maroon-700 to-maroon-900 p-6 text-center relative overflow-hidden shrink-0 shadow-md">
+                <div class="absolute inset-0 opacity-10">
+                    <svg class="h-full w-full" width="100%" height="100%" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+                        <rect width="100%" height="100%" fill="url(#grid)" />
+                    </svg>
                 </div>
+                
+                <div class="relative z-10 flex flex-col items-center justify-center">
+                    {{-- FIXED: Use route('home') instead of "/" --}}
+                    <a href="{{ route('home') }}">
+                        <x-application-logo class="w-16 h-16 fill-current text-white" />
+                    </a>
+                    <h1 class="text-white text-lg font-bold mt-2 tracking-wide">Think. Ask. Learn.</h1>
+                </div>
+            </div>
 
-                {{-- Form Container --}}
+            {{-- RIGHT SIDE: FORM PANEL --}}
+            <div class="w-full lg:w-1/2 flex flex-col justify-center items-center p-8 bg-white flex-grow">
                 <div class="w-full max-w-md space-y-6">
                     {{ $slot }}
                 </div>
-                
             </div>
         </div>
     </body>
